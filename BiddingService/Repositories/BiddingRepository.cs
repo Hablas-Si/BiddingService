0b3edf59fc63c6dd3b5aa4588683718aec3b6821
FynboNohr69420 <117977003+FynboNohr69420@users.noreply.github.com>
2024-05-16 09:22:47 +0200

    MongoImplemented

--- BiddingService/Repositories/BiddingRepository.cs
+++ BiddingService/Repositories/BiddingRepository.cs
@@ -4,6 +4,9 @@ using MongoDB.Driver;
 using MongoDB.Bson;
 using Microsoft.Extensions.Options;
 using BiddingService.Models;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text;
 
 namespace BiddingService.Repositories
 {
@@ -30,7 +33,7 @@ namespace BiddingService.Repositories
             return bid.Accepted;
         }
 
-        public async Task SubmitBid(Bid bid)
+        public async Task SubmitBid(Bid bid, Guid auctionID)
         {
             bid.Accepted = ValidateBid(bid);
 
@@ -38,6 +41,12 @@ namespace BiddingService.Repositories
             {
                 Console.WriteLine($"Bid submitted: {bid}");
                 await BidCollection.InsertOneAsync(bid);
+                var hBid = new HighBid
+                {
+                    userName = bid.BidOwner,
+                    Amount = bid.Amount
+                };
+                await PostToOtherService(hBid, auctionID);
             }
             else
             {
@@ -55,6 +64,45 @@ namespace BiddingService.Repositories
 
             //Return values
             return bids;
+        }
+
+        public async Task PostToOtherService(HighBid bid, Guid auctionID)
+        {
+            try
+            {
+                // Serialize the bid object into JSON
+                string jsonBid = JsonSerializer.Serialize(bid);
+
+                // Create an instance of HttpClient
+                using (var client = new HttpClient())
+                {
+                    // Set the content type header to "application/json"
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    // Construct the URL with auctionID using string interpolation
+                    string url = $"http://localhost:5188/Auction/{auctionID}/bid";
+
+                    // Make the PUT request to the endpoint of the other service
+                    var response = await client.PutAsync(url, new StringContent(jsonBid, Encoding.UTF8, "application/json"));
+
+                    // Check if the request was successful
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // Log the successful submission
+                        Console.WriteLine($"Bid submitted to other service for auction {auctionID}: {bid}");
+                    }
+                    else
+                    {
+                        // Log if the request failed
+                        Console.WriteLine($"Failed to submit bid to other service for auction {auctionID}: {bid}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log any exceptions that occur
+                Console.WriteLine($"An error occurred while submitting bid to other service: {ex.Message}");
+            }
         }
     }
 }
