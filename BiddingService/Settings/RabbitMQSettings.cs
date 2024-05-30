886a77801485125ba4abad0d25e08e2dfb2a92c5
FynboNohr69420 <117977003+FynboNohr69420@users.noreply.github.com>
2024-05-30 12:23:38 +0200

    rabbit connection matching azure

--- BiddingService/Settings/RabbitMQSettings.cs
+++ BiddingService/Settings/RabbitMQSettings.cs
@@ -2,7 +2,7 @@
 {
     public class RabbitMQSettings
     {
-        public string Hostname { get; set; } = "rabbitmq";
+        public string Hostname { get; set; } = "backend";
         public string QueueName { get; set; } = "BidToAuc";
     }
 }
